2d99b0406ec48d4bfa4856804735643957929a50
Prabodashan <prabodashan14@gmail.com>
2023-03-19 22:54:50 +0530

    added some sections

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ sr.reveal(".home-text", { delay: 350, origin: "left" });
 sr.reveal(".home-img", { delay: 350, origin: "right" });
 
 sr.reveal(
-  ".sub-service,.about,.portfolio,.service,.cta,.contact,.welcome, .contactus,.about-data, .careers, .our-team",
+  ".sub-service,.about,.portfolio,.service,.cta,.contact,.welcome, .contactus,.about-data, .careers, .our-team, .about2",
   {
     delay: 200,
     origin: "bottom",
